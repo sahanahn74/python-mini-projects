e23cd6d0d0a67354f963a0cd892a3af41dc6f9f7
sahanahn74 <160003715+sahanahn74@users.noreply.github.com>
2024-02-25 22:26:58 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#YouTuber ="kylie ying"
+#print("subscribe to "+YouTuber)
+#print("subscribe to {}".format(YouTuber))
+#print("subscribe to {youtuber}")
+
+adj = input("adjective:")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+
+madlib = f"computer programming is so{adj}! it makes me so excited all the time because \ I love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
